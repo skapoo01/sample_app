db221962f0fe2cdb32c75a890d6aea69f188051c
Siddharth Kapoor <Siddharth.Kapoor@tufts.edu>
2016-03-05 10:09:50 -0500

    finished user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
-  end
+  	
+  	def show
+  		@user = User.find(params[:id])
+  		# debugger
+  		# insert when need to debug anything
+  	end
+
+  	def new
+  		@user = User.new
+  	end
+
+  	def create
+  		# don't do mass assignments - too insecure - use "strong parameters" instead
+  		@user = User.new(user_params)
+  		if @user.save
+  			flash[:success] = "Welcome to the Sample App!"
+  			redirect_to @user
+  		else
+  			render 'new'
+  		end
+  	end
+
+  	private 
+
+  	def user_params 
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	end
 end
