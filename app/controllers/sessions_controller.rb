923f224c83f71f51c36067fa0117b924541bf6d2
Siddharth Kapoor <Siddharth.Kapoor@tufts.edu>
2016-03-07 12:27:29 -0500

    remember me checkbox working finally

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,15 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  		# Log the user in and redirect to the user's show page. 
+  	if user && user.authenticate(params[:session][:password]) # Log the user in and redirect to the user's show page. 
   		login user 
-  		(params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		redirect_to user
   	else 
   		flash.now[:danger] = 'Invalid email/password combination' # Create an error message.
   		render 'new'
     end
+
   end
 
   def destroy
